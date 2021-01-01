6861e0fcbc853307aa135eaf32eb9322f0feb71b
cchoijjinyoung <cgy7127@naver.com>
2021-01-01 19:26:16 +0900

    Add: eventBinding

---
+++ javascript/dom/scriptExam/index.js
@@ -0,0 +1,14 @@
+//window.onload = function() {
+  window.addEventListener("load", function(){
+  var btnPrint = document.getElementById("btn-print");
+  var add = function(x,y){
+    return x + y;
+  }
+  btnPrint.onclick = function () {
+    var x = prompt("x값 입력");
+    var y = prompt("y값 입력");
+    x = parseInt(x);
+    y = parseInt(y);
+    btnPrint.value = x + y;
+  };
+});
