ce792aa87aaf8687ccae79ca1bf2976e0b069d54
cchoijjinyoung <cgy7127@naver.com>
2021-01-05 23:34:32 +0900

    delButton

--- javascript/dom/scriptExam/javascript2-event.js
+++ javascript/dom/scriptExam/javascript2-event.js
@@ -1,7 +1,15 @@
 //연습문제 1-선택된 레코드 삭제하기:event target
 window.addEventListener("load", function(){
-  
-    
+
+  var section = document.querySelector("#section1-1");
+  var delButtons = section.querySelectorAll(".del-button")
+
+  for(var i=0; i<delButtons.length; i++) {
+      delButtons[i].onclick = function(e) {
+        var tr = e.target.parentElement.parentElement;
+        tr.remove();
+    };
+  }
 });
 //Ex1-선택된 이미지 보여주기:event target
 window.addEventListener("load", function(){
